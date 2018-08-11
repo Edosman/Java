f1435ef046a6620a0d31c23e81ae74644c6b018d
AndryKr <yandryx@ya.ru>
2018-08-12 02:42:49 +0300

    Java. Level 1. Lesson 1. HomeWork

---
+++ HomeWork1.java
@@ -0,0 +1,47 @@
+/**
+ * Java. Level 1. Lesson 1. Homework
+ *
+ * @author Andry Krasikov
+ * @version Aug 11, 2018
+ */
+
+public class HomeWork1 {
+    public static void main (String[] args){
+        int number = 1;
+        String name = "Андрей";
+        System.out.println(arithExpr (5,6,8,2));
+        System.out.println(limit10_20 (1,5));
+        positiveOrNegative (10);
+        System.out.println(negative (-36));
+        name (name);
+        //leapYear ((short) 1945);
+    }
+
+    public static int arithExpr (int a, int b, int c, int d) { //может и не int а float или double?
+        return a*(b+(c/d));
+    }
+
+    public static boolean limit10_20 (int x, int y) {
+        return (10<=x+y && 20>=x+y);
+    }
+
+    public static void positiveOrNegative (int number) {
+        if (number<0) {
+            System.out.println("Число отрицательное");
+        }
+        System.out.println("Число положительное");
+    }
+
+    public static boolean negative (int integer) {
+        return (integer < 0);
+    }
+	
+    public static void name (String name) {
+        System.out.println("Привет, " + name + "!");
+    }
+/*
+    public static void leapYear (short year) {
+
+    }
+*/
+}
