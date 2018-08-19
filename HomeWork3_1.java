099fe9e4902ea0935671d87d47e060556b651954
AndryKr <yandryx@ya.ru>
2018-08-19 20:12:35 +0300

    Добавил два файла, в каждом файле решение одной задачи.

---
+++ HomeWork3_1.java
@@ -0,0 +1,63 @@
+/**
+ * Java. Level 1. Lesson 3. Home work
+ *
+ * @author Andry Krasikov
+ * @version Aug 19, 2018
+ */
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class HomeWork3_1 {
+    public static void main(String[] args) {
+        riddle();
+    }
+
+    public static void riddle (){
+        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int digit = rand.nextInt(10);
+        System.out.println("Попытайтесь угадать целое число в пределах от 0 до 9 за 3 попытки.");
+        if (scanner.hasNextInt()){
+        for (int i = 0; i < 3; i++) {
+                int answer = scanner.nextInt();
+            if (answer >= 0 && answer < 10) {
+                if (digit == answer) {
+                    System.out.printf("Поздравляем! Вы угадали, компьютер загодал число %d! \n", digit);
+                    i = 3;
+                } else if (answer > digit) {
+                    System.out.println("Загаданное число меньше.");
+                } else {
+                    System.out.println("Загаданное число больше.");
+                }
+            } else {
+                System.out.println("Некорректный ввод");
+            }
+        }
+        replay();
+        scanner.close();
+        } else {
+            System.out.println("Некорректный ввод");
+            riddle ();
+        }
+    }
+
+    public static void replay (){
+        System.out.println("Игра закончилась. Повторить игру ещё раз? 1-да, 0-нет");
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextInt()) {
+            int replay = scanner.nextInt();
+            if (replay == 0) {
+                System.out.println("Конец игры!");
+            } else if (replay == 1) {
+                riddle();
+            } else {
+                System.out.println("Некорректный ввод");
+                replay();
+            }
+        } else {
+            System.out.println("Некорректный ввод");
+            replay();
+        }
+    }
+}
---
+++ HomeWork3_2.java
@@ -0,0 +1,55 @@
+/**
+ * Java. Level 1. Lesson 3. Home work
+ *
+ * @author Andry Krasikov
+ * @version Aug 19, 2018
+ */
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class HomeWork3_2{
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        riddle();
+    }
+
+    public static void riddle() {
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado",
+                "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak",
+                "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear",
+                "pepper", "pineapple", "pumpkin", "potato"};
+        System.out.println("Попытайтесь угадать слово, которое я загадал из списка ниже:\n");
+        System.out.println(Arrays.toString(words));
+        Random puzzle = new Random();
+        int i = puzzle.nextInt(words.length);
+        String answer;
+        do {
+            System.out.println("\nВведите слово:");
+            answer = scanner.nextLine();
+            if (answer.equals(words[i])) {
+                System.out.println("Вы угадали!");
+            } else {
+                int shortText = Math.min(answer.length(), words[i].length());
+                int x = 0;
+                for (int j = 0; j < shortText; j++) {
+                    if (answer.charAt(j) == words[i].charAt(j)) {
+                        System.out.print(answer.charAt(j));
+                        x++;
+                    } else {
+                        System.out.print("#");
+                    }
+                }
+                for (int k = 0; k < 15 - shortText; k++) {
+                    System.out.print("#");
+                }
+                System.out.printf("\nВ вашем слове есть %d совпадение(я) с загаданным словом\n", x);
+                System.out.println("\nВведите слово и попробуйте угадать ещё раз");
+            }
+        } while (!answer.equals(words[i]));
+        scanner.close();
+        System.out.println(words[i]);
+    }
+}
