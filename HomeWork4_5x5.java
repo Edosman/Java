d9b935ffe820edbac1009fb8be3b0d0644209a4f
AndryKr <yandryx@ya.ru>
2018-08-24 02:51:34 +0300

    Решение задачи 3 урока 4 Java1

---
+++ HomeWork4_5x5.java
@@ -0,0 +1,138 @@
+/**
+ * Java. Level 1. Lesson 3. Home work
+ *
+ * @author Andry Krasikov
+ * @version Aug 24, 2018
+ */
+
+import java.util.Random;
+import java.util.Scanner;
+
+class HomeWork3_2 {
+
+    final int SIZE = 5;
+    final int WIN_SIZE = 4;
+    final char DOT_X = 'x';
+    final char DOT_O = 'o';
+    final char DOT_EMPTY = '.';
+    char[][] map = new char[SIZE][SIZE];
+    Scanner sc = new Scanner(System.in);
+    Random rand = new Random();
+
+    public static void main(String[] args) {
+        new HomeWork3_2();
+    }
+
+    HomeWork3_2() {
+        initMap();
+        while (true) {
+            humanTurn();
+            if (checkWin(DOT_X)) {
+                System.out.println("YOU WON!");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("AI WON!");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+        }
+        System.out.println("GAME OVER.");
+        printMap();
+    }
+
+    void initMap() {
+        for (int i = 0; i < SIZE; i++)
+            for (int j = 0; j < SIZE; j++)
+                map[i][j] = DOT_EMPTY;
+    }
+
+    void printMap() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++)
+                System.out.print(map[i][j] + " ");
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Enter X and Y (1..5):");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y));
+        map[y][x] = DOT_X;
+    }
+
+    void aiTurn() {
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        map[y][x] = DOT_O;
+    }
+
+    boolean checkWin(char dot) {
+        int x = 0, y = 0, z = 0, w = 0;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == dot) {
+                    x++;
+                   if (x == WIN_SIZE) return true;
+                }
+                else {
+                    x=0;
+                }
+                if (map[j][i] == dot)
+                    y++;
+                if (y == WIN_SIZE) return true;
+                else{
+                    y=0;
+                }
+                if (i<SIZE-1 && j<SIZE-1) {
+                    if (map[i][j] == dot && map[i + 1][j + 1] == dot)
+                        z++;
+                    if (z == WIN_SIZE) return true;
+                    else{
+                        z=0;
+                    }
+                }
+                if (j>1 && i<SIZE-1) {
+                    if (map[i][j] == dot && map[i + 1][j - 1] == dot)
+                        w++;
+                    if (w == WIN_SIZE) return true;
+                    else{
+                        w=0;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++)
+            for (int j = 0; j < SIZE; j++)
+                if (map[i][j] == DOT_EMPTY)
+                    return false;
+        return true;
+    }
+
+    boolean isCellValid(int x, int y) {
+        if (x < 0 || y < 0 || x >= SIZE || y >= SIZE)
+            return false;
+        return map[y][x] == DOT_EMPTY; // by DSerov
+    }
+}
